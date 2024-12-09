c532b28997cef6b53486a4c61f75e1009d25ca0b
ShiLuxin <3220104390@zju.edu.cn>
2024-12-09 16:33:49 +0800

    通过函数传参“

--- server/utils/evaluate.py
+++ server/utils/evaluate.py
@@ -5,10 +5,11 @@ from torchvision.datasets import mnist
 from torch.utils.data import DataLoader
 
 
-def evaluate_mnist_models():
-    args = parse_args()
-
-    rate = args.rate
+def evaluate_mnist_models(rate=1.0,
+                          model_encoder="../../semantic_extraction/MLP_MNIST_encoder_combining_1.000000.pkl",
+                          model_classifier="../../semantic_extraction/MLP_MNIST.pkl",
+                          dataset_path="../../semantic_extraction/dataset/mnist",
+                          output_image_path="../../reconstruct_image"):
 
     raw_dim = 28 * 28  # shape of the raw image
     compression_rate = min((rate + 10) * 0.1, 1)
@@ -17,15 +18,17 @@ def evaluate_mnist_models():
     lambda2 = compression_rate
 
     mlp_encoder, mlp_mnist = load_models(
-        args.model_encoder, args.model_classifier, channel)
-    testset = mnist.MNIST(args.dataset_path, train=False,
+        model_encoder, model_classifier, channel)
+    testset = mnist.MNIST(dataset_path, train=False,
                           transform=data_transform, download=True)
     test_data = DataLoader(testset, batch_size=128, shuffle=False)
 
     out = test_model(mlp_encoder, mlp_mnist, test_data, lambda1, lambda2)
+    # print(out.shape)
 
-    save_recovered_images(out, args.output_image_path)
+    save_recovered_images(out, output_image_path)
 
 
 if __name__ == '__main__':
-    evaluate_mnist_models()
+    evaluate_mnist_models(1.0, "../../semantic_extraction/MLP_MNIST_encoder_combining_1.000000.pkl",
+                          "../../semantic_extraction/MLP_MNIST.pkl", "../../semantic_extraction/dataset/mnist", "../../reconstruct_image")
--- server/utils/reconstruct.py
+++ server/utils/reconstruct.py
@@ -1,14 +1,16 @@
 from prepare import *
+import os
 
 
-def reconstruct():
-    args = parse_args()
+def reconstruct(output_data_path="./compress_data", output_image_path="./reconstruct_image"):
+    os.makedirs(output_image_path, exist_ok=True)
 
-    compressed_np = np.load(f"{args.output_data_path}/compressed_data.npy")
+    compressed_np = np.load(f"{output_data_path}/compressed_data.npy")
     compressed_tensor = torch.from_numpy(compressed_np).float()
 
-    save_recovered_images(compressed_tensor, args.output_image_path)
+    save_recovered_images(compressed_tensor, output_image_path)
 
 
 if __name__ == '__main__':
-    reconstruct()
+    reconstruct("../../semantic_extraction/compress_data",
+                "../../reconstruct_image")
